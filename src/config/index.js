b2db7bbd566b55e5f7bd51cb4e0f67bacfe55885
halfrice <halfrice@pm.me>
2020-05-04 23:51:56 -0700

    Configure config 🧐

---
+++ src/config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  site: {
+    title: "Castle Inc.",
+    shortTitle: "Castle",
+    description: "A business site",
+    keywords: "",
+    url: "https://github.com/halfrice/castle-inc",
+    prefix: "/",
+    language: "en_US",
+  },
+  manifest: {
+    name: "Castle Inc.",
+    shortName: "Castle",
+    startUrl: "/",
+    backgroundColor: "#EAEBF3",
+    themeColor: "#FFFFFF",
+    display: "minimal-ui",
+    lang: "en",
+  },
+}
