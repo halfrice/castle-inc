b5c7f007238fc3f1dddda064c718585870e62f69
halfrice <halfrice@pm.me>
2020-05-05 00:07:51 -0700

    Scaffold search engine optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+const config = require("./src/config/index")
+
 module.exports = {
+  siteMetadata: {
+    title: config.site.title,
+    description: config.site.description,
+    url: config.site.url,
+  },
   plugins: [],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,37 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
 import "normalize.css/normalize.css"
+import { Seo } from "~components"
 import { Globals } from "~styles"
 
-const Layout = ({ children }) => (
-  <div id="layout">
-    <Globals />
-    {children}
-  </div>
-)
+const Layout = ({ children }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query LayoutQuery {
+          site {
+            siteMetadata {
+              description
+              title
+              url
+            }
+          }
+        }
+      `}
+      render={({ site }) => (
+        <div id="layout">
+          <Seo metadata={site.siteMetadata} />
+          <Globals />
+          {children}
+        </div>
+      )}
+    />
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export default Layout
